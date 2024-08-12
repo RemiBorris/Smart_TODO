c793e9d87ab6c40a3474154778590cf61779153e
rusgyn <rusgynmagsanay@gmail.com>
2024-08-12 16:48:48 -0400

    Modify tasks description property

--- db/schema/03_tasks.sql
+++ db/schema/03_tasks.sql
@@ -5,7 +5,7 @@ CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL,
   name VARCHAR(50) NOT NULL,
-  description VARCHAR(255) NOT NULL DEFAULT 'To Do',
+  description VARCHAR(255),
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   is_completed BOOLEAN NOT NULL DEFAULT FALSE
 );
