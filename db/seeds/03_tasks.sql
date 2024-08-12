7ebdf05f51a55aa3bb1eaa6216615dc725b45e4f
rusgyn <rusgynmagsanay@gmail.com>
2024-08-12 15:23:59 -0400

    Modify the tasks entity, add new property and set a DEFAULT.

--- db/seeds/03_tasks.sql
+++ db/seeds/03_tasks.sql
@@ -1,20 +1,20 @@
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (1, 'To watch Star Wars in order', 2);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (2, 'To eat Mediterranean food', 4);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (3, 'To eat Pizza', 4);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (3, 'To read Atomic Habits by James Clear', 1);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (2, 'To read Harry Potter by JK Rowlings', 1);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (3, 'To watch Games of Thrones', 2);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (1, 'To buy a new laptop', 3);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (3, 'To buy a new printer', 3);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (1, 'To wash the dishes', 5);
-INSERT INTO tasks (user_id, description, category_id)
+INSERT INTO tasks (user_id, name, category_id)
   VALUES (3, 'To play outside', 5);
