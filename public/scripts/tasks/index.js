906eb3c42a588d35b16e1bb7c2df909011cdce8a
rusgyn <rusgynmagsanay@gmail.com>
2024-08-14 23:35:50 -0400

    Implement mockup and scripts for users task list.

---
+++ public/scripts/tasks/index.js
@@ -0,0 +1,13 @@
+// Client facing scripts here
+// users task
+// Handle browser events
+
+// Add event listener for cancel button
+$('#cancel-btn').on('click', function() {
+  window.location.href = '/home'; // Redirect to the login page
+});
+
+
+$(() => {
+
+});
