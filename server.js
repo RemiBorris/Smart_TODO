9a6d064264b47bdc44ae810ec3d6c518c165a693
rusgyn <rusgynmagsanay@gmail.com>
2024-08-14 23:28:10 -0400

    Mount new routes called task. Edit root page to load home.

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const tasksRoutes = require('./routes/tasks');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -39,13 +40,13 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
-
+// app.use('/tasks', tasksRoutes);
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('home'); // was index
 });
 
 app.listen(PORT, () => {
