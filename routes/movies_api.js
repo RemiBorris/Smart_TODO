c480ca81c01733578ce9fa2c5a3402bab22ef380
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-08-17 23:43:31 -0700

    switch to tmdb api for isMovie function, increased flexibility

--- routes/movies_api.js
+++ routes/movies_api.js
@@ -3,43 +3,28 @@ const axios = require('axios');
 const MoviesApiKey = process.env.MoviesApiKey;
 
 
-//MOVIE API FUNCTION//
+// this function checks if the input is a movie //
 
 function isMovie(title) {
-  const encodedTitle = encodeURIComponent(title);
-  const url = `http://www.omdbapi.com/?apikey=${MoviesApiKey}&t=${encodedTitle}`
-
+  //remove 'watch' from title if present
+  const cleanedTitle = title.replace(/\bwatch\b/i, '').trim();
+  const encodedTitle = encodeURIComponent(cleanedTitle);
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${MoviesApiKey}&query=${encodedTitle}`;
   return axios.get(url)
-    .then(res => {
-      const data = res.data;
-       // Additional checks
-       const hasValidResponse = data.Response === 'True';
-       const isMovieType = data.Type === 'movie';
-       const hasValidYear = data.Year && data.Year.match(/^\d{4}$/); // Ensures it's a 4-digit year
-       const hasValidGenre = data.Genre && data.Genre.match(/(Action|Drama|Comedy|Adventure|Horror|Thriller)/i);
-       const hasMinimumRating = data.imdbRating && parseFloat(data.imdbRating) > 0;
-       const hasDirector = data.Director && data.Director !== 'N/A';
-       const hasPlot = data.Plot && data.Plot !== 'N/A';
-
-      if (
-        hasValidResponse &&
-        isMovieType &&
-        hasValidYear &&
-        hasValidGenre &&
-        hasMinimumRating &&
-        hasDirector &&
-        hasPlot
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching movie data:', error.message);
+  .then(res => {
+    const data = res.data;
+    if (data.results && data.results.length > 0) {
+      return true;
+    } else {
       return false;
-    });
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching movie data:', error.message);
+    return false;
+  });
 }
+
 
 //export module
 module.exports = { isMovie };
